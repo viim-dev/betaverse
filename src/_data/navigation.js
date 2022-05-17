4233f7c7fde821a10fd7927817cb6a5e4c987e3c
Lloyd <lloyd@emelle.me>
2022-05-17 17:35:05 -0400

    updates to locker

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,4 +1,5 @@
 module.exports = [
-  { name: 'Home', route: '/' },
-  { name: 'About', route: '/about/' },
+  { name: 'My NFTs', route: '/locker/nft' },
+  { name: 'My NFEs', route: '/locker/nfe' },
+  { name: 'Create', route: '/create/' },
 ]
