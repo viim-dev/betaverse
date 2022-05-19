66cfa971b67ed48a920f86fcd0576efbbfb97b21
Lloyd <lloyd@emelle.me>
2022-05-18 21:04:56 -0400

    updates

---
+++ functions/_middleware.js
@@ -0,0 +1,15 @@
+const errorHandler = async ({ next }) => {
+    try {
+      return await next();
+    } catch (err) {
+      return new Response(`${err.message}\n${err.stack}`, { status: 500 });
+    }
+  };
+  
+  const hello = async ({ next }) => {
+    const response = await next();
+    response.headers.set('Sec-FedCM-CSRF', '?1');
+    return response;
+  };
+  
+  export const onRequest = [errorHandler, hello];
