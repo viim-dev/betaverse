0a3340bc1884011027a9e5ebc2bd74b4ed5b447e
toby <lloyd@emelle.me>
2022-11-23 16:31:59 -0500

    reveal

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,6 +1,5 @@
 module.exports = [
-  { name: 'My NFTs', route: '/locker/nft' },
-  { name: 'My NFEs', route: '/locker/nfe' },
-  { name: 'Market Place', route: '/marketplace' },
-  { name: 'Create', route: '/create/' },
+  { name: 'AWARE', route: '/reveal' },
+  { name: 'Locker', route: '/locker' },
+  { name: 'Profile', route: '/profile/' },
 ]
