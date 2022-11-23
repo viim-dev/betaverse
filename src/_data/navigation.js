1ac32fca5bbc865f958e4131db7fdfcabc5500f2
toby <lloyd@emelle.me>
2022-11-23 16:44:42 -0500

    reveal

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,5 +1,5 @@
 module.exports = [
   { name: 'AWARE', route: '/reveal' },
   { name: 'Locker', route: '/locker' },
-  { name: 'Profile', route: '/profile/' },
+  { name: 'Profile', route: '/' },
 ]
